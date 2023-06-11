817955904250de9765ea0cdd478e985a024b5336
Mariam Soliman Kassab <123699968+Mariamkassab@users.noreply.github.com>
2023-06-11 20:38:47 +0300

    trial

--- hello.py
+++ hello.py
@@ -1,7 +1,31 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route("/")
 def hello():
-    return '<h1>Hello World, We are the Itian!</h1>'
+    return '''
+        <html>
+        <head>
+            <style>
+                /* Add CSS styles here */
+                body {
+                    background-color: lightblue;
+                }
+                h1 {
+                    color: red;
+                }
+                p {
+                    color: green;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>Hello World, We are the Itian!</h1>
+            <p>This is a colored paragraph.</p>
+        </body>
+        </html>
+    '''
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
